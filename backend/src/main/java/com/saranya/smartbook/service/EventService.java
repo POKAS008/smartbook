4ca553182cc0d57394209c1b060a111261b14dd9
Saranya <saranyaeng123@gmail.com>
2026-03-17 20:31:50 -0500

    made changes to emojis on admin page

--- backend/src/main/java/com/saranya/smartbook/service/EventService.java
+++ backend/src/main/java/com/saranya/smartbook/service/EventService.java
@@ -1,17 +1,21 @@
 package com.saranya.smartbook.service;
 
 import com.saranya.smartbook.model.Event;
+import com.saranya.smartbook.repository.BookingRepository;
 import com.saranya.smartbook.repository.EventRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
 public class EventService {
 
     private final EventRepository eventRepo;
+    private final BookingRepository bookingRepo;
 
-    public EventService(EventRepository eventRepo) {
-        this.eventRepo = eventRepo;
+    public EventService(EventRepository eventRepo, BookingRepository bookingRepo) {
+        this.eventRepo   = eventRepo;
+        this.bookingRepo = bookingRepo;
     }
 
     public List<Event> getAllEvents() {
@@ -30,7 +34,9 @@ public class EventService {
         return eventRepo.save(event);
     }
 
+    @Transactional
     public void deleteEvent(Long id) {
+        bookingRepo.deleteByEventId(id);
         eventRepo.deleteById(id);
     }
 }
